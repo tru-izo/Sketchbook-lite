ae67b8e12e31b319a21e4b48c73526047fe476ab
Tony Ruiz <truizo@outlook.com>
2021-01-17 10:05:45 -0500

    added js files

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 let size = 10;
+let color = "black";
 
 function draw(x, y) {
-    ctx.beginPath();
-        ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-        ctx.moveTo(110, 75);
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, Math.PI * 2); // Outer circle
+  ctx.moveTo(110, 75);
+  ctx.fillStyle = color;
+  ctx.fill();
 }
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
+
+draw(100, 200);
+drawLine(300, 300, 300, 500);
