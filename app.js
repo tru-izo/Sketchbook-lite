ce6e61454785d6c65e96f4e81b0854d919e34605
Tony Ruiz <truizo@outlook.com>
2021-01-19 10:48:00 -0500

    added events

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const canvas = document.getElementById("canvas");
+const increaseFont = document.getElementById("increase");
+const decreaseFont = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const brush = document.getElementById("brush");
+const eraser = document.getElementById("eraser");
+const clearBtn = document.getElementById("clear");
 const ctx = canvas.getContext("2d");
-let size = 10;
+let size = 5;
 let color = "black";
 let isPressed = false;
 let x;
 let y;
 
-canvas.addEventListener('mousedown', (e) =>{
+canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
-  x = e.offsetX
-  y = e.offsetY
-
+  x = e.offsetX;
+  y = e.offsetY;
 });
 
 canvas.addEventListener("mouseup", (e) => {
@@ -22,15 +28,15 @@ canvas.addEventListener("mouseup", (e) => {
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if(isPressed) {
+  if (isPressed) {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
-    
-    draw(x2, y2);
-    drawLine(x,y, x2,y2);
 
-    x = x2
-    y = y2
+    draw(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
   }
 });
 
@@ -49,3 +55,26 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+function updateSizeOnScreen(){
+  sizeEl.innerText = size;
+}
+
+//btn event listeners
+colorEl.addEventListener("change", (e) => color = e.target.value);
+
+increaseFont.addEventListener('click', () =>{
+  size += 2
+  if(size > 50){
+    size = 50
+  }
+  updateSizeOnScreen();
+})
+
+decreaseFont.addEventListener("click", () => {
+  size -= 2;
+  if (size < 2) {
+    size = 2;
+  }
+  updateSizeOnScreen();
+});
